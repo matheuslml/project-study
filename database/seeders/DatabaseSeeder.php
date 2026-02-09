dc93015fc55a18ddcb24469cda95fff46915df46
Matheus de Lima Mendonça <matheusm.uff@gmail.com>
2026-02-09 16:44:14 -0300

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
          $this->call(DisciplineSeeder::class);
          $this->call(LessonSeeder::class);
          $this->call(ExerciseSeeder::class);
+         $this->call(DisciplinePersonSeeder::class);
          //$this->call(UserSeeder::class);
     }
 }
